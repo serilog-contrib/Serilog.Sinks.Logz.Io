41143916020871706d0683995d6fd45b5561d96a
Mantas Audickas <mantas.audickas@gmail.com>
2025-08-06 20:11:43 +0300

    improve publish and Readme

--- publish.cmd
+++ publish.cmd
@@ -1,14 +1,12 @@
 @ECHO OFF
 
-set version1=7.2.0
-set version2=3.0.0
+set version1=8.0.0
+set version2=4.0.0
 
 set output=./packages
 
 dotnet restore .
 
-dotnet build . /p:Version=%version1% --configuration=Release --no-restore
-
-dotnet pack ./src/Serilog.Sinks.Logz.Io/Serilog.Sinks.Logz.Io.csproj -o %output% /p:Version=%version1% --configuration=Release --no-restore --no-build
-rem dotnet pack ./src/Serilog.Sinks.Http.LogzIo/Serilog.Sinks.Http.LogzIo.csproj -o %output% /p:Version=%version2% --configuration=Release --no-restore --no-build
+dotnet pack ./src/Serilog.Sinks.Logz.Io/Serilog.Sinks.Logz.Io.csproj -o %output% /p:Version=%version1% --configuration=Release --no-restore
+dotnet pack ./src/Serilog.Sinks.Http.LogzIo/Serilog.Sinks.Http.LogzIo.csproj -o %output% /p:Version=%version2% --configuration=Release --no-restore
 
